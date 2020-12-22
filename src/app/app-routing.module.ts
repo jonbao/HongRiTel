2d662d68fe2939ef1a0ca5d840cf2d845dd4acbf
Jon Bao <jon.baoq@gmail.com>
2020-12-22 00:01:13 -0600

    create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { CustomerListComponent } from './customer/customer-list/customer-list.component';
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
+  {
+		path: "CustomerList",
+		component: CustomerListComponent
+	},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
