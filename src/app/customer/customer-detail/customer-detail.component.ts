79a0ff61fd7356827cdcb98557b3baad2cd54532
Jon Bao <jon.baoq@gmail.com>
2021-01-05 16:37:17 -0600

    refactor list,operate,detail pages

--- src/app/customer/customer-detail/customer-detail.component.ts
+++ src/app/customer/customer-detail/customer-detail.component.ts
@@ -5,76 +5,10 @@ import { Observable, Observer } from 'rxjs';
 import { CustomerService } from "../customer.service";
 import { ActivatedRoute, Router} from "@angular/router";
 
-// @Component({
-//   selector: 'app-customer-operate',
-//   templateUrl: './customer-operate.component.html',
-//   styleUrls: ['./customer-operate.component.scss']
-// })
 @Component({
   selector: 'app-customer-detail',
-  template: `
-    <h2><span>客户信息管理</span></h2><br>
-    <form nz-form>
-    <nz-form-item>
-      <nz-form-label [nzSpan]="7" nzRequired>姓名</nz-form-label>
-      <nz-form-control [nzSpan]="15" nzHasFeedback style="word-wrap: break-word;word-break: break-all;overflow: hidden;">
-      {{ customer.name }}
-      </nz-form-control>
-    </nz-form-item>    
-    <nz-form-item>
-      <nz-form-label [nzSpan]="7" nzRequired>用户名称</nz-form-label>
-      <nz-form-control [nzSpan]="15" nzHasFeedback>
-      {{ customer.userName }} 
-      </nz-form-control>
-    </nz-form-item>
-    <nz-form-item>
-      <nz-form-label [nzSpan]="7" nzRequired>密码</nz-form-label>
-      <nz-form-control [nzSpan]="15" nzHasFeedback>
-      {{ customer.password }}
-      </nz-form-control>
-    </nz-form-item>
-    <nz-form-item>
-      <nz-form-label [nzSpan]="7">电话</nz-form-label>
-      <nz-form-control [nzSpan]="15" nzHasFeedback >
-      {{ customer.phoneNumber }}
-      </nz-form-control>
-    </nz-form-item>    
-    <nz-form-item>
-      <nz-form-label [nzSpan]="7" nzRequired>Email</nz-form-label>
-      <nz-form-control [nzSpan]="15" nzHasFeedback>
-      {{ customer.email }}
-      </nz-form-control>
-    </nz-form-item>
-    <nz-form-item>
-      <nz-form-label [nzSpan]="7">联系渠道</nz-form-label>
-      <nz-form-control [nzSpan]="15">
-      {{ customer.contactNote }}
-      </nz-form-control>
-    </nz-form-item>
-    <nz-form-item>
-      <nz-form-label [nzSpan]="7">备注</nz-form-label>
-      <nz-form-control [nzSpan]="15">
-      {{ customer.remark }}
-      </nz-form-control>
-    </nz-form-item>    
-    <nz-form-item>
-      <nz-form-control [nzOffset]="7" [nzSpan]="15">
-        <button nz-button nzType="primary" nzSize="large"(click)="Cancel()">Cancel</button>
-      </nz-form-control>
-    </nz-form-item>
-  </form>
-  `,
-  styles: [
-    `
-      [nz-form] {
-        max-width: 800px;
-      }
-      [nz-button] {
-        margin-right: 8px;
-        margin-bottom: 12px;
-      }     
-    `
-  ]
+  templateUrl: './customer-detail.component.html',
+  styleUrls: ['./customer-detail.component.scss']
 })
 export class CustomerDetailComponent implements OnInit {
   public customer: any = {};
--- src/app/customer/customer-operate/customer-operate.component.ts
+++ src/app/customer/customer-operate/customer-operate.component.ts
@@ -5,85 +5,10 @@ import { Observable, Observer } from 'rxjs';
 import { ActivatedRoute, Router} from "@angular/router";
 import { CustomerService } from "../customer.service";
 
-// @Component({
-//   selector: 'app-customer-operate',
-//   templateUrl: './customer-operate.component.html',
-//   styleUrls: ['./customer-operate.component.scss']
-// })
 @Component({
   selector: 'app-customer-operate',
-  template: `
-    <h2><span>客户信息管理</span></h2><br>
-    <form nz-form [formGroup]="validateForm" (ngSubmit)="submitForm(validateForm.value)">
-    <nz-form-item>
-      <nz-form-label [nzSpan]="7" nzRequired>姓名</nz-form-label>
-      <nz-form-control [nzSpan]="15" nzHasFeedback nzErrorTip="请输入姓名!">
-        <input nz-input formControlName="Name" [(ngModel)]="customer.name" />
-      </nz-form-control>
-    </nz-form-item>    
-    <nz-form-item>
-      <nz-form-label [nzSpan]="7" nzRequired>用户名称</nz-form-label>
-      <nz-form-control [nzSpan]="15" nzHasFeedback nzErrorTip="请输入用户名!">
-        <input nz-input formControlName="UserName" [(ngModel)]="customer.userName" />
-      </nz-form-control>
-    </nz-form-item>
-    <nz-form-item>
-      <nz-form-label [nzSpan]="7" nzRequired>密码</nz-form-label>
-      <nz-form-control [nzSpan]="15" nzHasFeedback nzErrorTip="请输入密码!">
-        <input nz-input formControlName="Password" [(ngModel)]="customer.password" />
-      </nz-form-control>
-    </nz-form-item>
-    <nz-form-item>
-      <nz-form-label [nzSpan]="7">电话</nz-form-label>
-      <nz-form-control [nzSpan]="15" nzHasFeedback nzErrorTip="请输入电话!">
-        <input nz-input formControlName="PhoneNumber" [(ngModel)]="customer.phoneNumber" />
-      </nz-form-control>
-    </nz-form-item>    
-    <nz-form-item>
-      <nz-form-label [nzSpan]="7" nzRequired>Email</nz-form-label>
-      <nz-form-control [nzSpan]="15" nzHasFeedback [nzErrorTip]="emailErrorTpl">
-        <input nz-input formControlName="Email" placeholder="email" type="email" [(ngModel)]="customer.email" />
-        <ng-template #emailErrorTpl let-control>
-          <ng-container *ngIf="control.hasError('email')">
-            The input is not valid E-mail!
-          </ng-container>
-          <ng-container *ngIf="control.hasError('required')">
-            Please input your E-mail!
-          </ng-container>
-        </ng-template>
-      </nz-form-control>
-    </nz-form-item>
-    <nz-form-item>
-      <nz-form-label [nzSpan]="7">联系渠道</nz-form-label>
-      <nz-form-control [nzSpan]="15" nzErrorTip="Please write something here!">
-        <textarea formControlName="ContactNote" nz-input rows="3" placeholder="请输入联系渠道！" [(ngModel)]="customer.contactNote"></textarea>
-      </nz-form-control>
-    </nz-form-item>
-    <nz-form-item>
-      <nz-form-label [nzSpan]="7">备注</nz-form-label>
-      <nz-form-control [nzSpan]="15" nzErrorTip="Please write something here!">
-        <textarea formControlName="Remark" nz-input rows="3" placeholder="请输入备注！" [(ngModel)]="customer.remark"></textarea>
-      </nz-form-control>
-    </nz-form-item>    
-    <nz-form-item>
-      <nz-form-control [nzOffset]="7" [nzSpan]="15">
-        <button nz-button nzType="primary" nzSize="large" [disabled]="!validateForm.valid">Submit</button>
-        <button type="button" nz-button nzSize="large" (click)="Cancel()">Cancel</button>
-      </nz-form-control>
-    </nz-form-item>
-  </form>
-  `,
-  styles: [
-    `
-      [nz-form] {
-        max-width: 600px;
-      }
-      [nz-button] {
-        margin-right: 8px;
-        margin-bottom: 12px;
-      }      
-    `
-  ]
+  templateUrl: "./customer-operate.component.html",
+  styleUrls: ["./customer-operate.component.scss"]
 })
 export class CustomerOperateComponent implements OnInit {
   validateForm: FormGroup;
