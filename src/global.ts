190a5adae10f77b2844adb168769f4c559339516
Jon Bao <jon.baoq@gmail.com>
2021-01-18 00:31:16 -0600

    update global variable

--- src/global.ts
+++ src/global.ts
@@ -1,6 +1,6 @@
 export const GlobalVariable = Object.freeze({
-    BASE_API_URL: 'http://localhost:1234/',
-    //BASE_API_URL: 'http://82.156.163.158:801/',
+    //BASE_API_URL: 'http://localhost:1234/',
+    BASE_API_URL: 'http://82.156.163.158:801/',
     
     //... more of your variables
 });
